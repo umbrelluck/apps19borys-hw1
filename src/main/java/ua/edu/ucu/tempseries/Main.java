e8735b68e32e1e030a4ed5e1839efebcb59fe89a
umbrelluck <yarko.umbrelluck@gmail.com>
2019-10-10 23:11:03 +0300

    Added class implementation

--- src/main/java/ua/edu/ucu/tempseries/Main.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ucu.tempseries;
-
-public class Main {
-    public static void main(String[] args) {
-        double[] rg=new double[5];
-        rg[0]=5.0;
-        System.out.println(rg.length);
-    }
-}
